082ef445683aaa7f89da255f510855fe3c861a41
cnrd <hello@conradweise.com>
2020-06-17 23:48:43 +0200

    added study

--- interfaces/distribution/main.js
+++ interfaces/distribution/main.js
@@ -3,22 +3,26 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY25yZCIsImEiOiJjanY2ZWF0ZHAwMGs3NDNxcjI0MjVob
 const locations = [
   [6.942628, 50.928259],
   [6.960028, 50.921341],
-  [6.981778, 50.938389]
+  [6.981778, 50.938389],
+  [6.957563, 50.921615],
+  [6.957563, 50.921615],
+  [6.957563, 50.921615],
+  [6.957563, 50.921615],
+  [6.957563, 50.921615]
 ]
 
 let counter = 1
 
 locations.forEach((location) => {
-
   const map = new mapboxgl.Map({
     container: `map-${ counter }`,
     style: 'mapbox://styles/mapbox/light-v10',
     center: location,
-    zoom: 17,
+    zoom: 16,
     attributionControl: false
   })
 
-  map.on('load', function() {
+  map.on('load', () => {
     map.addSource('points', {
       type: 'geojson',
       data: {
